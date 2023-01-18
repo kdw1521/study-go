e69d1604b7747b5aa5f0087b4d73f40040913bb7
wando <kdw40161217@gmail.com>
2023-01-18 09:48:21 +0900

    feat: 타입변환 정수형

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import "fmt"
 
 func main() {
 	var number1 int = 10
-	var number2 int
-	var number3 = 20
-	number4 := 100
+	var number2 float64 = 33.5
 
-	fmt.Println(number1, number2, number3, number4)
+	var number3 int = int(number2) // number2는 float64이므로 int type으로 형변환해서 할당, int로 변환하기떄문에 소수는 사라짐
+
+	number4 := float64(number1) * number2
+
+	var number5 int64 = 20
+	number6 := number1 * int(number5)
+
+	fmt.Println(number1, number2, number3, number4, number5, number6)
 }
