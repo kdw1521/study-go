e0b77e26ba1c3b1c999de0f0312e1a79621fcd3f
wando <kdw40161217@gmail.com>
2023-02-20 21:19:18 +0900

    feat: study interface first #14

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "study-go/study/method"
+import "study-go/study/wando_interface/minju"
+
+// 이제 여기서 wando 말고 minju로 보내고 싶으면 코드를 바꿔야한다..!
+func SendBook(name string, sender *minju.MinjuSender) {
+	sender.Send(name)
+}
 
 func main() {
-	method.Embedded_Runner()
+	sender := &minju.MinjuSender{}
+	SendBook("완도 일대기", sender)
+	SendBook("완도 일대기2", sender)
 }
